16e470254c47e73922f61ed5e82064ef026b5681
i-goldman <panthergold74@gmail.com>
2017-10-27 23:22:03 -0400

    Add files via upload

--- obsidem_sounds.py
+++ obsidem_sounds.py
@@ -3,13 +3,36 @@ import subprocess
 import serial
 from operator import xor
 
-s = serial.Serial('/dev/tty.usbserial',9600)
+s = serial.Serial("/dev/ttyACM0")
 len_data = 15
+
 data_in = [False for i in range (len_data)]
 
 while True:
-    for i in range (0, len_data):
-         data_in[i] = xor(s.readline() , data_in[i] )
-    if data_in (0):
-        subprocess.Popen('vlc s0.mp3')
-        
+    event = -1;
+    old_data = data_in
+    if s.readline() == 'S':
+        for j in range (len_data):
+            data_in[j] = s.readline()
+            if XOR(data_in[j] == old_data[j]):
+                    event = j
+
+        if event == 0: p=subprocess.Popen(["vlc","/home/obsidem/Desktop/SoundFX/0"])
+        if event == 1: p=subprocess.Popen(["vlc","/home/obsidem/Desktop/SoundFX/0"])
+        if event == 2: p=subprocess.Popen(["vlc","/home/obsidem/Desktop/SoundFX/0"])
+        if event == 3: p=subprocess.Popen(["vlc","/home/obsidem/Desktop/SoundFX/0"])
+        if event == 4: p=subprocess.Popen(["vlc","/home/obsidem/Desktop/SoundFX/0"])
+        if event == 5: p=subprocess.Popen(["vlc","/home/obsidem/Desktop/SoundFX/0"])   
+        if event == 6: p=subprocess.Popen(["vlc","/home/obsidem/Desktop/SoundFX/0"])
+        if event == 7: p=subprocess.Popen(["vlc","/home/obsidem/Desktop/SoundFX/0"])
+        if event == 8: p=subprocess.Popen(["vlc","/home/obsidem/Desktop/SoundFX/0"])
+        if event == 9: p=subprocess.Popen(["vlc","/home/obsidem/Desktop/SoundFX/0"])
+        if event == 10: p=subprocess.Popen(["vlc","/home/obsidem/Desktop/SoundFX/0"])
+        if event == 11: p=subprocess.Popen(["vlc","/home/obsidem/Desktop/SoundFX/0"])
+        if event == 12: p=subprocess.Popen(["vlc","/home/obsidem/Desktop/SoundFX/0"])
+        if event == 13: p=subprocess.Popen(["vlc","/home/obsidem/Desktop/SoundFX/0"])
+        if event == 14: p=subprocess.Popen(["vlc","/home/obsidem/Desktop/SoundFX/0"])
+        if event == 15: p=subprocess.Popen(["vlc","/home/obsidem/Desktop/SoundFX/0"])
+
+
+
